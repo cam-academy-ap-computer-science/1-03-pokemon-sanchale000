17b1379907bf68dad55089cf785312d141b32237
sanchale000 <sanchale000@CAM-LAB-24.bgsd.k12.wa.us>
2019-09-09 07:19:27 -0700

    Assignment

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,9 +1,12 @@
 
 public class Pokemon {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^.^)");
+		System.out.println("z(_(\") (\")");
 
+		
 	}
 
 }
